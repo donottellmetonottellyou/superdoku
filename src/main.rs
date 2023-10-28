7306670ef9bb7ff3d38c78c8daf5af8b5281270f
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-10-28 10:50:39 -0400

    set up scaffold for CLI version

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod cli;
+mod gui;
+
+use clap::Parser;
+
+/// Superdoku, a Sudoku solver
+#[derive(Parser)]
+#[command(author, version)]
+pub struct Args {
+    /// Use the command line interface version of Superdoku
+    #[arg(short, long)]
+    pub cli: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.cli {
+        cli::main();
+    } else {
+        gui::main();
+    }
+}
