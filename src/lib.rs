03d64c2d1b072e6a7047aff0b1ccce46c68c24f7
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-11-10 18:00:00 -0500

    Add testing to library (#11)
    * removed anyhow as it's not being used anymore
    
    * tested that the individual tiles correctly display
    
    * remove unnecessary slicing in test
    
    * reorder tests in lib.rs
    
    * moved board_display to tests/
    because the internal api is not needed for testing display
    
    * actually removed the test in lib.rs this time
    
    * merge in fixes to updates in undo method

--- src/lib.rs
+++ src/lib.rs
@@ -232,30 +232,6 @@ mod tests {
     use std::collections::HashSet;
 
     #[test]
-    fn square_terminal_representation_looks_right() {
-        let correct_display = vec![
-            "                            \n",
-            "  |-------|-------|-------| \n",
-            "9 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "8 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "7 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "  |-------|-------|-------| \n",
-            "6 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "5 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "4 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "  |-------|-------|-------| \n",
-            "3 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "2 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "1 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "  |-------|-------|-------| \n",
-            "    a b c   d e f   g h i   \n",
-        ]
-        .concat();
-
-        assert_eq!(correct_display, format!("{}", Board::default()));
-    }
-
-    #[test]
     fn find_neighbor_locations_finds_correct_locations() {
         // 5e or e5
         let location = (4, 4);
