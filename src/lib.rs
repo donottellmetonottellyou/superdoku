ed6bd2a99919e1d14720c7e71da2981a08c59307
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-11-10 16:51:44 -0500

    actually removed the test in lib.rs this time

--- src/lib.rs
+++ src/lib.rs
@@ -227,28 +227,4 @@ mod tests {
             HashSet::from(Board::find_neighbor_locations(location))
         );
     }
-
-    #[test]
-    fn square_terminal_representation_looks_right_1() {
-        let correct_display = vec![
-            "                            \n",
-            "  |-------|-------|-------| \n",
-            "9 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "8 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "7 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "  |-------|-------|-------| \n",
-            "6 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "5 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "4 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "  |-------|-------|-------| \n",
-            "3 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "2 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "1 | ? ? ? | ? ? ? | ? ? ? | \n",
-            "  |-------|-------|-------| \n",
-            "    a b c   d e f   g h i   \n",
-        ]
-        .concat();
-
-        assert_eq!(correct_display, format!("{}", Board::default()));
-    }
 }
